51aec8a9c6dce19a9021451c5dd9dfa5b5844877
Jerie <jerie.stolbergrohr@gmail.com>
2025-04-05 20:23:42 +0200

    now it works

--- js/form.js
+++ js/form.js
@@ -1,7 +1,11 @@
 const slider = document.querySelector ("#minder");
 const minderValue = document.querySelector ("#range-value");
 slider.style.accentColor = "#81e6b5";
-slider.addEventListener("input", minderValue);
+slider.addEventListener("input", updateValue);
+
+function updateValue() {
+    minderValue.textContent = slider.value;
+}
 
 /*summerey*/
 const submit = document.querySelector ("#submit");
