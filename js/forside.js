6d6060f51e6f9916d43a9f56268c9adaf2f77d9d
Jerie <jerie.stolbergrohr@gmail.com>
2025-04-09 11:44:24 +0200

    modal

---
+++ js/forside.js
@@ -0,0 +1,24 @@
+/*nr 2*/
+document.querySelector ("#lm_1").addEventListener("click", openDia1);
+function openDia1(e) {
+    e.preventDefault();
+    document.querySelector ("#dialog_1").showModal();
+}
+/*nr 2*/
+document.querySelector("#lm_2").addEventListener("click", openDia2);
+function openDia2(e) {
+    e.preventDefault();
+    document.querySelector("#dialog_2").showModal();
+}
+/*nr 3*/
+document.querySelector("#lm_3").addEventListener("click", openDia3);
+function openDia3(e) {
+    e.preventDefault();
+    document.querySelector("#dialog_3").showModal();
+}
+/*nr 4*/
+document.querySelector("#lm_4").addEventListener("click", openDia4);
+function openDia4(e) {
+    e.preventDefault();
+    document.querySelector("#dialog_4").showModal();
+}
