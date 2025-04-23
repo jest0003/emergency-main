a857689d185fe95d7e3d6c52f87835ca19ffedf2
Jerie <jerie.stolbergrohr@gmail.com>
2025-04-23 08:44:34 +0200

    darkmode

---
+++ js/darkmode.js
@@ -0,0 +1,17 @@
+const switcher = document.querySelector("#switcher");
+const css = document.querySelector("#lightmode");
+console.log (css.href);
+
+const btn = document.querySelector ("#switchBtn");
+btn.addEventListener ("click", change);
+
+function change () {
+    if (css.href.includes("darkmode")) {
+        css.href = "../css/custom.css";
+        switcher.textContent = "Dark mode";
+    }
+    else {
+        css.href = "../css/darkmode.css";
+        switcher.textContent = "Light mode";
+    }
+}
